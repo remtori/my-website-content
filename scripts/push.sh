ccc62a6354ad3b696d0e243a1dbeca1f2c490f67
Remtori <lqv.remtori@gmail.com>
2020-02-05 02:54:07 +0700

    Fix recursive build

--- scripts/push.sh
+++ scripts/push.sh
@@ -8,6 +8,6 @@ git stash
 git checkout master
 git stash pop
 git add .
-git commit -m "Travis CI: Update generated"
+git commit -m "[travis-ci skip] Update generated/*"
 git push https://${GITHUB_TOKEN}@github.com/remtori/my-website-content.git
 echo "Done !"
